caa2c963de606cd8c2d1bfba60061334f3531441
David Cramer <dcramer@gmail.com>
2011-12-09 15:01:00 -0800

    Bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ tests_require = [
 
 setup(
     name='nose-bleed',
-    version='0.3.1',
+    version='0.4.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     description='A progressive coverage plugin for Nose.',
