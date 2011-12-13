ef3f3fe128853cc589d0508a8d857bdadaef8f23
David Cramer <dcramer@gmail.com>
2011-12-13 11:16:56 -0800

    Initial test skeletons

---
+++ tests/db/tests.py
@@ -0,0 +1,19 @@
+from unittest2 import TestCase
+
+from kleenex.db import CoverageDB
+
+import logging
+import os.path
+
+class CoverageDBTest(TestCase):
+    def setUp(self):
+        self.db = CoverageDB('sqlite:///test.db', logger=logging.getLogger(__name__))
+        self.db.upgrade()
+
+    def tearDown(self):
+        os.unlink('test.db')
+
+    def test_has_seen(self):
+        self.assertFalse(self.db.has_seen_test('foo.bar'))
+        self.db.set_test_has_seen_test('foo.bar', '1')
+        self.assertTrue(self.db.has_seen_test('foo.bar'))
